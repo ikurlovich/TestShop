e99b10d4c7f68783aa4bc4990ead73713f928e97
ikurlovich <kia555587937@gmail.com>
2023-03-08 22:39:15 +0400

    Initial Commit

---
+++ TestShop/TestShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestShopApp.swift
+//  TestShop
+//
+//  Created by Илья Курлович on 08.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
